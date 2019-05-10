03c50b0eaf91876840c1a6df8769251a372561d5
Christian Scott <christian@oaktree.systems>
2019-05-10 17:39:58 -0400

    Added instructions

--- source-anchor.ps1
+++ source-anchor.ps1
@@ -1,6 +1,8 @@
 #PeopleIT
 #
 #Use this script to find the on-premise AD account source anchor for a user when trying to match them to their MS Online user. Once you get the source anchor, you can run the set-msoluser -UserPrincipalName <email address> -ImmutableId <id from this script> to make sure there is a hard match when they are moved to the syncing OU.
+Read-Host -prompt 'Welcome. You can use this script to retrieve and update the Source Anchor for a user who exists in On-premise AD and MS Online (Office 365). This script is particularly useful when you need to update the source anchor of a user's 365 (online) directory account to match with a local source anchor, so that when you add them to a synching OU, a hard match occurs in Office365. Please make sure you update the user's UPN and proxy addresses to match whatever is in Office365 Exchange Online Today. You will be prompted for Office365 tenant credentials next. Please make sure you are running this on the domain controller with Azure AD Sync installed.
+
 Connect-MsolService
 
 # Name of the person you are looking for in AD
